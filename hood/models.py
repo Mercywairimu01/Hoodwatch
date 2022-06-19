7a202aef8c938d7b83364a76d2ba38e476be6e20
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-19 12:29:20 +0300

    add profile class

--- hood/models.py
+++ hood/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -27,11 +28,32 @@ class NeighbourHood(models.Model):
     
     @classmethod
     def update_neighborhood(cls,neighborhood_id,location):
-      return cls.objects_filter(id=neighborhood_id).update(location=location)
+      return cls.objects.filter(id=neighborhood_id).update(location=location)
 
     @classmethod
     def update_occupants(cls,neighborhood_id,occupant_count):
-        return cls.objects_filter(id=neighborhood_id).update(occupant_count)
+        return cls.objects.filter(id=neighborhood_id).update(occupant_count)
+    
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    about = models.TextField(max_length=254, blank=True)
+    image = models.ImageField(upload_to='images/', default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, null=True, related_name='members')
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+    def save(self, **kwargs):
+        super().save( **kwargs)
+        img= Image.open(self. image.path)
+        if img.height > 250 or img.width > 250:
+            output_size = (250, 2500)
+            img.thumbnail(output_size)
+            img.save(self. image.path)
+
+ 
 
 
 
