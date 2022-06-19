ea688c74da7b6d620dea88229142b4fb2ecb1125
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-19 13:56:46 +0300

    add views for the profile

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render,redirect
+from .models import *
+from .forms import *
+from django.contrib import messages
 # Create your views here.
+def index(request):
+    neigh = NeighbourHood.objects.all()
+    neigh =neigh[::-1]
+    return render(request,'index.html',{ 'neigh':neigh})
+
+def register(request):
+    if request.method == 'POST':
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            # password=form.cleaned_data.get('password')
+
+            return redirect('login')
+    else:
+        form=RegistrationForm()
+    return render(request, 'register/registration.html',{'form':form})
+
+
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserForm(request.POST, instance=request.user)
+        p_form = ProfileForm(request.POST,request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account updated!')
+            return redirect('profile')
+    else:
+        u_form = UserForm(instance=request.user)
+        p_form = ProfileForm(instance=request.user.profile)
+        current_profile = Profile.objects.get(user_id = request.user)
+        current_post = Post.user_post(request.user)
+        
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'current_profile':current_profile,
+        'current_post':current_post,
+    }
+    return render(request, 'register/profile.html', context)
